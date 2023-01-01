7a55c206ce5ed48f15a907cf7ad3519d5dfd0a8d
dhayalan.balaraman <dhayalanit@gmail.com>
2023-01-01 19:12:19 +0530

    First commit

---
+++ src/main/java/com/dhayalanit/kafka_avro_schema_registry/KafkaAvroSchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.dhayalanit.kafka_avro_schema_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroSchemaRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAvroSchemaRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhayalanit/kafka_avro_schema_registry/KafkaAvroSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhayalanit.kafka_avro_schema_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroSchemaRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
